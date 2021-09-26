f34ebbc6506c71a75f34b846d7a12caae02815a9
paulomujuru <allelevou@gmail.com>
2021-09-26 23:27:37 +0200

    MEDIA_ROOT

--- calendar_api/urls.py
+++ calendar_api/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from .views import calendarView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('<year>', calendarView.as_view(), name="calendar")
 ]
